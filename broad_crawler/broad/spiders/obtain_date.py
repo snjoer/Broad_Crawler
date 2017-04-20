4e76ea5e9080023cf06bc0bcc0e6b684a8d765f4
jessywuzq <jessywuzq@gmail.com>
2017-04-20 21:14:39 +0800

    improve the way of obtaining date

--- broad_crawler/broad/spiders/obtain_date.py
+++ broad_crawler/broad/spiders/obtain_date.py
@@ -4,41 +4,64 @@
 import re
 import sys
 
-
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
 
-def isTime(d):
-    strLen = len(d)
+def isDate(hypo_date):
+    strLen = len(hypo_date)
     digit_count = 0
-    for dd in d:
-        if dd.isdigit():
+    for v in hypo_date:
+        if v.isdigit():
             digit_count += 1
-    if 30 > strLen > 5 and digit_count * 1.0 / strLen > 0.5 and digit_count > 8:
+    if 30 > strLen > 5 and 0.5 < digit_count * 1.0 / strLen < 0.9 and digit_count > 8:
         return 1
     else:
         return 0
 
 
-def pre_process(times):
+def find_first_digit_index(hypo_date):
+    for k, v in enumerate(hypo_date):
+        if v.isdigit():
+            return k+1
+    else:
+        return 0
+
+
+def pre_process(dates):
     # \u5e74\u6708\u65e5
-    # times = [re.subn(r'[\u4e00-\u5e73]|[\u5e75-\u6707]', '', tm)[0] for tm in times]
-    # times = [re.subn(r'[\u6709-\u65e4]|[\u65e6-\u9fa5]', '', tm)[0] for tm in times]
-    times = times[:int(len(times) / 2)]
-    times = [tm.replace('\n', '').replace('\t', '').replace('\b', '').replace('&nbsp;', '') for tm in times]
-    times = [tm for tm in times if 100 > len(tm) > 0]
-    return times
+    # dates = [re.subn(r'[\u4e00-\u5e73]|[\u5e75-\u6707]', '', tm)[0] for tm in dates]
+    # dates = [re.subn(r'[\u6709-\u65e4]|[\u65e6-\u9fa5]', '', tm)[0] for tm in dates]
+    # 取一半
+    dates = dates[:int(len(dates) / 2)]
+    # 取长度小于100部分
+    dates = [d for d in dates if 100 > len(d) > 0]
+    # 去除一些特殊符号
+    dates = [d.replace('\n', ' ').replace('\t', ' ').replace('\b', ' ').replace('&nbsp;', ' ') for d in dates]
+    # 去除前后空格，合并多空格为一空格
+    new_dates = []
+    for v in dates:
+        v = re.sub('\A\s*', '', v)
+        v = re.sub('\s*\Z', '', v)
+        v = re.sub('\s+', ' ', v)
+        new_dates.append(v)
+    # 只取有数字的部分且去除该部分首位数字之前的内容
+    shorter_dates = []
+    for v in new_dates:
+        ffdi = find_first_digit_index(v)
+        if ffdi:
+            shorter_dates.append(v[ffdi-1:])
+    return shorter_dates
 
 
-def h1index(title, times):
+def h1index(title, dates):
     find_hi_index = 1
-    index = len(times) - 1
-    times = times[::-1]
+    index = len(dates) - 1
+    dates = dates[::-1]
     i = 0
     while i < index and find_hi_index:
         try:
-            a = times[i][0]
+            a = dates[i][0]
         except IndexError:
             print 'ERROR index', i
         else:
@@ -48,7 +71,8 @@ def h1index(title, times):
         i += 1
     return i
 
-def obtain_t(response):
+
+def obtain_d(response):
     data = response.body
     try:
         chatset = response.encoding
@@ -57,14 +81,13 @@ def obtain_t(response):
         print e
         raise UnicodeDecodeError
     else:
-        times = re.findall(r'>[\s\n]*(.*?)[\s\n]*<', data)
-        times = pre_process(times)
+        dates = re.findall(r'>[\s\n]*(.*?)[\s\n]*<', data)
+        shorter_dates = pre_process(dates)
         start_i = 0
-        # start_i = h1index(title,times)
-        for i in range(start_i, len(times)):
-            tm = times[i]
-            if isTime(tm):
-                return tm
-                break
+        # start_i = h1index(title,shorter_dates)
+        for i in range(start_i, len(shorter_dates)):
+            d = shorter_dates[i]
+            if isDate(d):
+                return d
         else:
-            return "none"
+            return ''
